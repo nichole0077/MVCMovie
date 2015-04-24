757757ce87fb9a8ba0ebf762915a3099053af18f
Nichole Carlson <nichole0077@gmail.com>
2015-04-24 13:31:07 -0500

    MVCMovie
    Need to complete: Add new field, etc

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //// 
+        //// GET: /HelloWorld/ 
+
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        //// 
+        //// GET: /HelloWorld/Welcome/ 
+
+        //public string Welcome(string name, int ID = 1)
+        //{
+        //    return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
+        //}
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+            
+
+            return View();
+        }
+    }
+} 
